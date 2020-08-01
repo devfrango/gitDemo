b522dd1dd91fb6e1d02c8043a622cd168be37a13
Frankie(devfrango) <devfrango@gmail.com>
2020-08-02 02:35:27 +0700

    add BookARoom.java

---
+++ src/BookARoom.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class BookARoom {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Do you want to book a room?");
+        String answer = scan.next();
+    }
+}
