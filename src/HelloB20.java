3b9c7587228727fbb22f868c3c7ef554c4a6d0e5
Frankie(devfrango) <devfrango@gmail.com>
2020-08-01 22:51:11 +0700

    update HelloB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,6 +1,6 @@
 public class HelloB20 {
 
-    public static void main(String[] args){
-        System.out.println("Hello B20, it is my pleasure to be with you!");
+    public static void main(String[] args) {
+        System.out.println("Hello, this is an app!");
     }
 }
