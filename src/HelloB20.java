113fd1b4fce961a90531ae9d63d0f37d1b7c78c5
Frankie(devfrango) <devfrango@gmail.com>
2020-08-02 02:11:42 +0700

    Modified HappyB20.java, HelloB20.java, and HelloB20Again.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,6 +1,6 @@
 public class HelloB20 {
 
     public static void main(String[] args) {
-        System.out.println("Hello, this is an app!");
+        System.out.println("Hello, this is an app!!!");
     }
 }
--- src/HelloB20Again.java
+++ src/HelloB20Again.java
@@ -1,6 +1,7 @@
 public class HelloB20Again {
 
     public static void main(String[] args) {
-        System.out.println("Hello again evryone~ ");
+        System.out.println("Hello again evryone");
+
     }
 }
