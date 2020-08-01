b23f681aec148184f607d9e88b18da657f7937e2
devfrango <47091755+devfrango@users.noreply.github.com>
2020-08-02 03:02:42 +0700

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -5,7 +5,7 @@ public class BookARoom {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Do you want to book a room?");
+        System.out.println("Do you want to book a room? (yes/no)");
         String answer = scan.next();
     }
 }
